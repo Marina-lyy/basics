e4630c849f1113dade27b9a8ed78047d6f0fcf52
lvyuyu <1295278001@qq.com>
2019-04-28 18:40:20 +0800

    lvyuyu

---
+++ 2-高级-包模块/p05.py
@@ -0,0 +1,4 @@
+from p01 import *
+stu = Student("yaona"20)
+stu.say()
+sayHello()
---
+++ 2-高级-包模块/pkg02/p01.py
@@ -0,0 +1,16 @@
+
+class Student():
+    def __init__(self,name="NoName",age=18):
+        self.name = name
+        self.age = age
+
+    def say(self):
+        print("My name is {0}".format(self.name))
+
+def sayHello():
+    print("Hi,欢迎来的图灵学院：！")
+
+
+
+
+print("我是模块p01呀，你特么的叫我干毛")
